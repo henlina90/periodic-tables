275aa4ea5b7d9c977eef37fa20e883b8902a90db
henlina90 <henlina90>
2021-12-14 19:08:52 -0800

    created migrations and seeded some data

---
+++ back-end/src/db/seeds/00-reservations.js
@@ -0,0 +1,7 @@
+const reservations = require("../data/00-reservations");
+
+exports.seed = function (knex) {
+  return knex
+    .raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE")
+    .then(() => knex("reservations").insert(reservations));
+};
